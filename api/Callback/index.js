0cef2448b67de89c00ffaef61757a3ffd023f916
Kieran Ross <kieran.j.ross@gmail.com>
2020-11-19 19:34:14 +0000

    add logging

--- api/Callback/index.js
+++ api/Callback/index.js
@@ -2,21 +2,23 @@ var fetch = require('node-fetch');
 var uuid = require('uuid');
 
 module.exports = async function (context, req) {
-
+    context.log("callback invoked")
     var code = req.query.code || null
     var state = req.query.state || null
     var cookies = req.headers.cookie ? parseCookie(req.headers.cookie) : null
-    var storedState = cookies.spotify_auth_state ? cookies.spotify_auth_state: null
+    var storedState = cookies.spotify_auth_state ? cookies.spotify_auth_state : null
 
     if (state === null || state !== storedState) {
+        context.log("State mismatch")
         res = {
             status: 302,
             headers: {
-                'Location': '/#'.concat(new URLSearchParams({authstatus: 'state_mismatch'}))
+                'Location': '/#'.concat(new URLSearchParams({ authstatus: 'state_mismatch' }))
             }
         }
+        context.log(res)
         context.done(null, res);
-      }
+    }
 
     const data = {
         client_id: process.env["SpotifyClientId"],
@@ -24,10 +26,10 @@ module.exports = async function (context, req) {
         redirect_uri: process.env['SpotifyRedirectUri'],
         grant_type: 'authorization_code',
         code: code
-        };
+    };
 
     const json = await getToken(new URLSearchParams(data));
-    
+
     // TODO: wrap in async function
     const containerClient = BlobServiceClient.fromConnectionString(process.env["SecurityStorage"]).getContainerClient(process.env["ContainerName"])
     await containerClient.createIfNotExists()
@@ -41,7 +43,7 @@ module.exports = async function (context, req) {
     res = {
         status: 302,
         headers: {
-            'Location': '/#'.concat(new URLSearchParams({authstatus: 'success'}))
+            'Location': '/#'.concat(new URLSearchParams({ authstatus: 'success' }))
         },
         body: null,
         cookies: [
@@ -58,18 +60,18 @@ module.exports = async function (context, req) {
     context.done(null, res);
 }
 
-async function getToken(params) {       
-    return fetch('https://accounts.spotify.com/api/token', {method: 'POST', body: params})
-    .then(res => res.json());
+async function getToken(params) {
+    return fetch('https://accounts.spotify.com/api/token', { method: 'POST', body: params })
+        .then(res => res.json());
 }
 
 // TODO: place in shared utils .js
-function parseCookie (str) {
-   return str
-    .split(';')
-    .map(v => v.split('='))
-    .reduce((acc, v) => {
-      acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
-      return acc;
-    }, {})
+function parseCookie(str) {
+    return str
+        .split(';')
+        .map(v => v.split('='))
+        .reduce((acc, v) => {
+            acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+            return acc;
+        }, {})
 }
